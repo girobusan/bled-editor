35d8d868ff818aa982c06669689cfbbecc72e456
girobusan <somewhere@the.rainbow>
2020-01-27 17:44:37 +0300

    init

---
+++ tested.js
@@ -0,0 +1,26 @@
+function editor(params){
+    /*
+    {
+    selector: "#id",
+    blocks: [..,...,..],
+    ...
+    }
+    */
+   this.element = d3.select(params.selector);
+   
+   this.addNewBlock = function(b){
+
+   }
+
+
+}
+
+function paragraphBlock(){
+
+    //render new empty block
+
+    //render block with data
+
+    //render ...
+
+}
