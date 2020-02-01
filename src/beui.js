e5a799356447de2d159cecea7bffd08280b364ea
Gleb <girobusan@gmail.com>
2020-02-01 22:37:39 +0300

    ask function

--- src/beui.js
+++ src/beui.js
@@ -11,19 +11,26 @@ icons.link = require("./svg/text_link.svg");
 
 export var mycyan = "#3ED9E3";
 
-export function tooltips(){
+export function Ask(pr) {
+    return new Promise(function (resolve, reject) {
+        let r = prompt(pr);
+        if (r) { resolve(r) } else { reject("No input") };
+    })
+}
+
+export function tooltips() {
     let tt = document.createElement("div");
     tt.style.position = "absolute";
     tt.style.display = "none";
     tt.style.zIndex = 20;
-    tt.style.pointerEvents = "none";    
+    tt.style.pointerEvents = "none";
 
     let ttin = document.createElement("div");
     tt.appendChild(ttin);
     ttin.style.backgroundColor = "rgba(100%, 100%, 100%, 0.8)";
     ttin.style.color = "#888888";
     ttin.style.pointerEvents = "none";
-    ttin.style.fontSize="12px";
+    ttin.style.fontSize = "12px";
     ttin.style.padding = "4px 8px";
     ttin.style.position = "relative";
     ttin.style.borderRadius = "4px";
@@ -33,25 +40,25 @@ export function tooltips(){
 
     document.body.appendChild(tt);
 
-    window.addEventListener("mousemove" , function(e){
+    window.addEventListener("mousemove", function (e) {
         let ttb = tt.getBoundingClientRect().height;
         tt.style.top = (e.clientY - ttb + window.scrollY) + "px";
         tt.style.left = e.clientX + "px";
 
     })
-        window.addEventListener("mouseover" , function(e){
-        if(e.target&&e.target.dataset.hint){
-        ttin.innerHTML = e.target.dataset.hint;
-       // tt.style.top = e.clientY + "px";
-          //  tt.style.left = e.clientX + "px";
-        tt.style.display = "block";
-        }else{            
+    window.addEventListener("mouseover", function (e) {
+        if (e.target && e.target.dataset.hint) {
+            ttin.innerHTML = e.target.dataset.hint;
+            // tt.style.top = e.clientY + "px";
+            //  tt.style.left = e.clientX + "px";
+            tt.style.display = "block";
+        } else {
             tt.style.display = "none";
         }
     })
 }
 
-export function textTools(){
+export function textTools() {
     let ttools = document.createElement("div");
     ttools.style.minWidth = "100px";
     ttools.classList.add("text_toolbox");
@@ -59,47 +66,47 @@ export function textTools(){
     ttools.style.backgroundColor = mycyan;
     ttools.style.position = "absolute";
     ttools.style.display = "none";
-    ttools.style.padding="0px 4px";
+    ttools.style.padding = "0px 4px";
     console.log("append text tools")
     document.body.appendChild(ttools);
     //buttons
-    function addButton(lbl,func , hint){
+    function addButton(lbl, func, hint) {
         let b = document.createElement("div");
         b.style.display = "inline-block";
         b.innerHTML = lbl;
         b.style.width = "18px";
-        b.style.height="18px";
+        b.style.height = "18px";
         b.style.overflow = "hidden";
-        b.addEventListener("mousedown" , func);
+        b.addEventListener("mousedown", func);
         b.classList.add("text_toolbox");
         b.style.cursor = "pointer";
         b.style.padding = "4px";
         let sv = b.querySelector("svg");
-        sv.style.pointerEvents ="none";//.style.pointerEvents("none");
-        if(hint){b.dataset.hint = hint};
+        sv.style.pointerEvents = "none";//.style.pointerEvents("none");
+        if (hint) { b.dataset.hint = hint };
         ttools.appendChild(b);
     }
 
-    addButton(icons.bold , function(e){
-        console.log("bold" , document.getSelection())
+    addButton(icons.bold, function (e) {
+        console.log("bold", document.getSelection())
         document.execCommand("bold");
         e.preventDefault();
-    } , "Bold")
-    addButton(icons.italic , function(e){
-        console.log("italic" , document.getSelection())
+    }, "Bold")
+    addButton(icons.italic, function (e) {
+        console.log("italic", document.getSelection())
         document.execCommand("italic");
         e.preventDefault();
-    } , "Italic")
-    addButton(icons.underline , function(e){
+    }, "Italic")
+    addButton(icons.underline, function (e) {
         //console.log("italic" , document.getSelection())
         document.execCommand("underline");
         e.preventDefault();
-    } , "Underline")
-    addButton(icons.strike , function(e){
+    }, "Underline")
+    addButton(icons.strike, function (e) {
         //console.log("italic" , document.getSelection())
         document.execCommand("strikeThrough");
         e.preventDefault();
-    } , "Strike")
+    }, "Strike")
     /*
     addButton("small" , function(e){
         console.log("small" , document.getSelection())
@@ -107,45 +114,44 @@ export function textTools(){
         e.preventDefault();
     })
     */
-    addButton(icons.sup , function(e){
-       // console.log("italic" , document.getSelection())
+    addButton(icons.sup, function (e) {
+        // console.log("italic" , document.getSelection())
         document.execCommand("superscript");
         e.preventDefault();
-    } , "Superscript")
-    addButton(icons.sub , function(e){
+    }, "Superscript")
+    addButton(icons.sub, function (e) {
         // console.log("italic" , document.getSelection())
-         document.execCommand("subscript");
-         e.preventDefault();
-     } , "Subscript")
-    addButton(icons.link , function(e){
-        let ur = prompt("enter URL");
-        if(!ur){ return }
-        //console.log("link" , document.getSelection())
-        document.execCommand("createLink" , false ,  unescape(ur));
+        document.execCommand("subscript");
         e.preventDefault();
-    } , "Make link")
+    }, "Subscript")
+    addButton(icons.link, function (e) {
+        Ask("Enter URL")
+            .then(r => document.execCommand("createLink", false, unescape(r)))
+            .catch(r=>console.log(r));
+        e.preventDefault();
+    }, "Make link")
 
     //
 
-    document.body.addEventListener("click" , function(e){
+    document.body.addEventListener("click", function (e) {
         //console.log(e.target.getAttribute("contenteditable"));
-    if(e.target.getAttribute("contenteditable") && !e.target.dataset.no_text_toolbox){
-        //console.log("click" , ttools);
-        let tgt = e.target.getBoundingClientRect();
-        ttools.style.left = tgt.left + "px";
-        ttools.style.display = "flex";
-        let tthe = ttools.getBoundingClientRect().height;
-        ttools.style.top = (tgt.top-tthe + window.scrollY) + "px";
-        
-    }else if(e.target.classList.contains("text_toolbox")){
-        //ttools.style.display = "block";
-    }else{
-        ttools.style.display = "none";
-    }
+        if (e.target.getAttribute("contenteditable") && !e.target.dataset.no_text_toolbox) {
+            //console.log("click" , ttools);
+            let tgt = e.target.getBoundingClientRect();
+            ttools.style.left = tgt.left + "px";
+            ttools.style.display = "flex";
+            let tthe = ttools.getBoundingClientRect().height;
+            ttools.style.top = (tgt.top - tthe + window.scrollY) + "px";
+
+        } else if (e.target.classList.contains("text_toolbox")) {
+            //ttools.style.display = "block";
+        } else {
+            ttools.style.display = "none";
+        }
     });
-     
- 
-    
+
+
+
 }
 export function addPlusButton(block, menu) {
     block.style.position = "relative";
@@ -262,13 +268,13 @@ export function addPlusButton(block, menu) {
 
 }
 
-export function addBlockControls(block, items , ed) {
-    if (!items&&ed) {
+export function addBlockControls(block, items, ed) {
+    if (!items && ed) {
         items = [
             {
                 label: "move block up",
                 icon: "🡡",
-                handler: function () { ed.moveUp(block.dataset.block_id)}
+                handler: function () { ed.moveUp(block.dataset.block_id) }
             },
             {
                 label: "move block down",
@@ -281,12 +287,12 @@ export function addBlockControls(block, items , ed) {
                 handler: function () { ed.removeBlock(block.dataset.block_id) }
             }
         ]
-    }else{
-        items=[];
+    } else {
+        items = [];
     }
     //
     block.style.position = "relative";
-    let ourclass = "ctrls"+block.dataset.block_id;
+    let ourclass = "ctrls" + block.dataset.block_id;
     let ctrls = document.createElement("div");
     ctrls.classList.add("common_block_controls");
     ctrls.classList.add(ourclass);
@@ -298,11 +304,11 @@ export function addBlockControls(block, items , ed) {
     ctrls.style.color = "white";
     ctrls.style.textAlign = "center";
     ctrls.style.display = "none";
-    ctrls.addEventListener("mouseover", () => {ctrls.style.zIndex=6 ; ctrls.style.display = "block"} );
-    ctrls.addEventListener("mouseout", () => {ctrls.style.zIndex="initial" ;  ctrls.style.display = "none"});
+    ctrls.addEventListener("mouseover", () => { ctrls.style.zIndex = 6; ctrls.style.display = "block" });
+    ctrls.addEventListener("mouseout", () => { ctrls.style.zIndex = "initial"; ctrls.style.display = "none" });
 
-    block.addEventListener("mouseover", () => { ctrls.style.zIndex=5 ; ctrls.style.display = "block"});
-    block.addEventListener("mouseout", () => { ctrls.style.zIndex="initial" ; ctrls.style.display = "none"});
+    block.addEventListener("mouseover", () => { ctrls.style.zIndex = 5; ctrls.style.display = "block" });
+    block.addEventListener("mouseout", () => { ctrls.style.zIndex = "initial"; ctrls.style.display = "none" });
 
 
 
